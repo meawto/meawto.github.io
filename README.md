8276bc5b81b32fceec987d0e77c6ba3dde98dadf
Milton Fidelis <coffeemiltonfidelis01@hotmail.com>
2021-01-11 06:34:57 -0300

    readme update; 'polishing' and other code changes

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# [meawto: home](https://meawto.github.io/)
+# [<h1 style="font-size: 3rem; text-align: center; font-weight: 100; color: #450052" onmouseover="this.style.color = '#c100e6'" onmouseout="this.style.color = '#450052'">NOTO OS</h1>](https://meawto.github.io/)
 
-not available for mobile devices;
+[<img alt="NOTO OS preview" src="https://i.imgur.com/uzmxyny.png" onmouseover="this.style.transform = 'scale(1.03)'; this.style.boxShadow = '0 0.6rem 1.2rem black';" onmouseout="this.style.transform = 'scale(1)'; this.style.boxShadow = '0 0.6rem 1.2rem transparent'" style="border-radius: 0.8rem; box-shadow: 0 0.6rem 1.2rem transparent; display: block; margin: 0 auto; margin-bottom: 6rem; transition: transform 100ms ease-out, box-shadow 200ms">](https://meawto.github.io/)
 
-unresponsive site (for now);
+###Português
+**Projeto em progresso**. É possível que você passe por **problemas de instabilidades** e por isso recomendo o uso do [Google Chrome](https://www.google.com/chrome/) v`87.0.4280.88`;
+Atualmente **disponível em português** e **futuramente em inglês**;
 
+ ### Engrish
+**Project in progress**. You may **experience problems**. I recommend using [Google Ghrome](https://www.google.com/chrome/) v`87.0.4280.88`;
+Currently **available in Portuguese**, available in **English in the future**;
 
-tested on google chrome 87.0.4280.88;
+> simulating the interface of an operating system with web technologies (yes, this is to be a portfolio)
 
-> working in progress
+<h5 style="border-left: 0.25rem solid red; padding: 0.4rem 1rem; color: var(--color-text-tertiary);"> I'm a beginner developer.</h5>
--- window.js
+++ window.js
@@ -2,7 +2,7 @@ let desktopSize = document.getElementById('os-desktop')
 
 function createWindow(name) {
 
-    var osWindow = document.getElementById(name)
+    let osWindow = document.getElementById(name)
 
     if (!(osWindow.style.opacity == '1')) {
 
@@ -12,11 +12,11 @@ function createWindow(name) {
         setTimeout(() => { osWindow.style.opacity = '1' }, 400)
     } else {
         if (osWindow.style.zIndex == '1') {
-            $(".os-window").not(this).css("z-index", "1")
-            $(".os-window").not(this).css("filter", "grayscale(1)")
+            // $(".os-window").css("z-index", "1")
+            // $(".os-window").css("filter", "grayscale(1)")
 
-            osWindow.style.zIndex = '2'
-            osWindow.style.filter = 'grayscale(0)'
+            // osWindow.style.zIndex = '2'
+            // osWindow.style.filter = 'grayscale(0)'
             osWindow.style.animation = ''
             void osWindow.offsetHeight
             osWindow.style.animation = 'showWindow 400ms'
@@ -27,29 +27,30 @@ function createWindow(name) {
 
 
 
-    $(".os-window").not(this).css("z-index", "1")
-    $(".os-window").not(this).css("filter", "grayscale(1)")
-    $(".os-window").not(this).css("box-shadow", "none")
-    $(".os-window").not(this).css("transform", "scale(0.98)")
-    $(".os-window .iframe-overlay").not(this).css("display", "block")
+    $(".os-window").css("z-index", "1")
+    $(".os-window").css("filter", "grayscale(1)")
+    $(".os-window").css("box-shadow", "none")
+    $(".os-window").css("transform", "scale(0.98)")
+    $(".iframe-overlay").css("display", "block")
 
 
     osWindow.style.zIndex = '2'
     osWindow.style.filter = 'grayscale(0)'
-    osWindow.style.filter = 'grayscale(0)'
     osWindow.style.boxShadow = '0 10px 30px var(--window-shadow-v1)'
     osWindow.style.transform = 'scale(1)'
-    osWindow.querySelector('.iframe-overlay').style.display = "none"
+    osWindow.querySelector('.iframe-container .iframe-overlay').style.display = "none"
 }
 
 function closeWindow(name) {
-    var osWindow = document.getElementById(name)
+    let osWindow = document.getElementById(name)
+
     osWindow.style.transition = 'opacity 400ms, top 1s ease, filter 400ms, box-shadow 200ms, transform 100ms'
     osWindow.style.opacity = '0'
     osWindow.style.top = desktopSize.offsetHeight + 'px'
 }
 
 
+// remove top 1s
 document.querySelectorAll('.os-window').forEach(item => {
     item.addEventListener('mousedown', () => {
         item.style.transition = 'opacity 400ms, filter 400ms, box-shadow 200ms, transform 100ms'
@@ -64,16 +65,17 @@ let windowsJquery = ["#music-player", "#notoryu-app"]
 windowsJquery.forEach(window => {
     $(window).draggable({
         containment: "parent",
-        tolerance: "touch",
         cursor: "crosshair",
         cancel: '.iframe-overlay, .chat-input',
         start: function() {
-            $(this).css("transform", "scale(1.02)");
-            $(this).css("opacity", "0.8");
+            $(this).css("transform", "scale(1.02)")
+            $(this).css("opacity", "0.8")
+            $(".iframe-overlay").css("display", "block")
         },
         stop: function() {
-            $(this).css("transform", "scale(1)");
+            $(this).css("transform", "scale(1)")
             $(this).css("opacity", "1")
+            $(".iframe-overlay").css("display", "none")
         }
     })
     $(window).css("top", desktopSize.offsetHeight)
@@ -85,12 +87,11 @@ $(".os-window").mousedown(function() {
     $(".os-window").not(this).css("filter", "grayscale(1)")
     $(".os-window").not(this).css("box-shadow", "none")
     $(".os-window").not(this).css("transform", "scale(0.98)")
-    $(".os-window .iframe-overlay").not(this).css("display", "block")
-    console.log('a')
+    $(".os-window .iframe-container .iframe-overlay").not(this).css("display", "block")
 
     $(this).css("z-index", "2")
     $(this).css("filter", "grayscale(0)")
     $(this).css("box-shadow", "0 10px 30px var(--window-shadow-v1)")
     $(this).css("transform", "scale(1)")
-    $(this).children(".iframe-overlay").css("display", "none")
+    $(this).children(".iframe-container").children(".iframe-overlay").css("display", "none")
 })
